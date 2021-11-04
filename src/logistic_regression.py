1b23ebb26481a8ab3c53046c66da2b37972ecd5a
Stjepan Salatovic <s.salatovic@yahoo.de>
2021-11-04 18:57:29 +0100

    Initial logistic regression.

---
+++ src/logistic_regression.py
@@ -0,0 +1,52 @@
+"""
+FYS-STK4155 @UiO, PROJECT II.
+Logistic regression.
+"""
+import numpy as np
+
+from src.activations import sigmoid
+from src.utils import acc
+from tqdm import tqdm
+
+
+sigmoid, _ = sigmoid()
+
+
+def sgd(data, n_epochs, batch_size, lr, reg):
+    X_train, X_test, y_train, y_test = data
+
+    n_batches = X_train.shape[0] // batch_size
+    idx = np.arange(X_train.shape[0])
+
+    beta = np.random.randn(X_train.shape[1])
+
+    def df(beta, X, y):
+        pred = sigmoid(X @ beta)
+        return X.T @ (pred - y) / y.size + 2 * reg * beta
+
+    hist = {"Train": [], "Test": []}
+    t = tqdm(range(n_epochs))
+    for _ in t:
+        np.random.shuffle(idx)
+        for b in range(n_batches):
+            batch = idx[b * batch_size: (b + 1) * batch_size]
+            beta -= lr * df(beta, X_train[batch], y_train[batch])
+
+        t.set_postfix(test_acc = acc(sigmoid(X_test @ beta) > 0.5, y_test))
+
+        hist["Train"].append(acc(sigmoid(X_train @ beta) > 0.5, y_train))
+        hist["Test"].append(acc(sigmoid(X_test @ beta) > 0.5, y_test))
+    return beta, hist
+
+
+class LogisticRegression:
+    def fit(self, data, n_epochs, batch_size, lr, reg):
+        self.beta, self.hist = sgd(data, n_epochs, batch_size, lr, reg)
+        return self
+
+    def predict(self, X):
+        return sigmoid(X @ self.beta) > 0.5
+
+    def score(self, X, y):
+        self.acc_test = acc(self.predict(X), y)
+        return self.acc_test
