213dbd9576c6858508e1a7c67fe7e4afe2a77d98
Stjepan Salatovic <s.salatovic@yahoo.de>
2021-11-20 09:50:19 +0100

    Test advection equation w/ ThreePoint FDM scheme.

--- test/test_finite_differences.py
+++ test/test_finite_differences.py
@@ -2,11 +2,11 @@
 FYS-STK4155 @UiO
 Testing: Finite differences.
 """
-from project3.exercises.ex_b import SHOW_PLOTS
+import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
-from src.finite_differences import FivePoint
+from src.finite_differences import ThreePoint, FivePoint
 
 
 SHOW_PLOTS = True
@@ -48,9 +48,36 @@ def solver_heat(c, u0, space, time):
     return RF
 
 
-def test_inital_condition(solver_heat, u0):
+@pytest.fixture(scope='session')
+def solver_adv(c, u0, space, time):
+    """
+    Solves the advection equation with the FDM
+    (Implicit Euler + 1. order upwind)
+    and returns the solver object.
+    """
+    RF = ThreePoint((0, np.array([-1, 1, 0])))
+    RF.solve(c, u0(space), space, time)
+    if SHOW_PLOTS:
+        RF.plot_solution()
+    return RF
+
+
+def test_heat_eq_inital_condition(solver_heat, u0):
     assert np.all(solver_heat.sol[:, 0] == u0(solver_heat.space))
 
 
-def test_boundary_condition(solver_heat):
+def test_heat_eq_boundary_condition(solver_heat):
     assert np.allclose(solver_heat.sol[[0, -1]], np.zeros((2, solver_heat.N + 1)), atol=1e-2)
+
+
+def test_adv_eq_conservation(solver_adv):
+    row_sum = solver_adv.sol.sum(axis=0)
+    assert np.allclose(row_sum, row_sum[0] * np.ones(row_sum.size))
+
+
+def test_adv_eq_initial_condition(solver_adv, u0):
+    assert np.all(solver_adv.sol[:, 0] == u0(solver_adv.space))
+
+
+def test_adv_eq_boundary_condition(solver_adv):
+    assert np.allclose(solver_adv.sol[0], solver_adv.sol[-1], atol=1E-1)
