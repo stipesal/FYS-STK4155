4a6f6805224690c516b46b58ad3fdf37154fe19d
Stjepan Salatovic <s.salatovic@yahoo.de>
2021-11-21 16:27:11 +0100

    Introduce 'EigenNet' for solving eigenvalue problems. Make class structure clearer. Type hints.

--- src/neural_pde.py
+++ src/neural_pde.py
@@ -2,48 +2,113 @@
 FYS-STK4155 @UiO
 Neural network for solving PDEs.
 """
+from __future__ import annotations
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from torch import nn
 
+from matplotlib.axes import Axes
+from matplotlib.image import AxesImage
+from torch import nn
+from torch import Tensor
 from torch.autograd import grad
 from tqdm import trange
-
+from typing import Callable, List, Tuple, Optional
 from src.utils import LABEL_SIZE
 
 
-class PDENet(nn.Module):
-    def __init__(self, units, activation):
+N = 1000
+N_TEST = 100
+
+
+class ANN(nn.Module):
+    def __init__(self, units: List[int], activation: Callable):
         super().__init__()
         layers = []
         for n, m in zip(units[:-1], units[1:]):
             layers.extend([nn.Linear(n, m), activation()])
         self.layers = nn.Sequential(*layers[:-1])
 
-    def set_problem(self, c, initial_condition, space, T):
+    def train(self, n_epochs: int, batch_size: int, lr: float) -> ANN:
+        train_data, test_data = self.sample_data()
+
+        n_batches = train_data[0].size(dim=0) // batch_size
+        idx = np.arange(train_data[0].size(dim=0))
+
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+
+        self.hist = {"Train": [], "Test": []}
+        epoch_range = trange(n_epochs, desc="Learning PDE")
+        for _ in epoch_range:
+            np.random.shuffle(idx)
+            for b in range(n_batches):
+                batch = idx[b * batch_size: (b + 1) * batch_size]
+
+                batch_train_data = [x[batch] for x in train_data]
+
+                loss = self.loss(*batch_train_data)
+                loss.backward()
+
+                optimizer.step()
+                optimizer.zero_grad()
+
+            train_loss, test_loss = self.eval(train_data, test_data)
+            epoch_range.set_postfix(train=train_loss, test=test_loss)
+        return self
+
+    def eval(self, train_data: Tuple[Tensor], test_data: Tuple[Tensor]) -> Tuple[float, float]:
+        train_loss = self.loss(*train_data).item()
+        test_loss = self.loss(*test_data).item()
+        self.hist["Train"].append(train_loss)
+        self.hist["Test"].append(test_loss)
+        return train_loss, test_loss
+
+
+class PDE_Net(ANN):
+    def __init__(self, units: List[int], activation: Callable):
+        super().__init__(units, activation)
+
+    def forward(self, x: Tensor, t: Tensor) -> Tensor:
+        input = torch.hstack((x.reshape(-1, 1), t.reshape(-1, 1)))
+        return self.layers(input)
+
+    def sample_data(self) -> Tuple[Tuple[Tensor], Tuple[Tensor]]:
+        xl, xr = self.space
+        x_train = (xl + (xr - xl) * torch.rand(N, 1)).requires_grad_(True)
+        t_train = (self.T * torch.rand(N, 1)).requires_grad_(True)
+        x_test = (xl + (xr - xl) * torch.rand(N_TEST, 1)).requires_grad_(True)
+        t_test = (self.T * torch.rand(N_TEST, 1)).requires_grad_(True)
+        return (x_train, t_train), (x_test, t_test)
+
+    def plot_solution(self, ax: Optional[Axes] = None) -> AxesImage:
+        if ax is None:
+            _, ax = plt.subplots()
+        n = 100
+        space = torch.linspace(*self.space, n)
+        time = torch.linspace(0, self.T, n)
+
+        space_, time_ = torch.meshgrid(space, time, indexing="ij")
+        ext = [time.min(), time.max(), space.min(), space.max()]
+
+        pred = self(space_.reshape(-1, 1), time_.reshape(-1, 1)).detach()
+        im = ax.imshow(pred.reshape(n, n), extent=ext, aspect="auto", origin="lower", cmap="coolwarm")
+        ax.set_xlabel(r"$t$", size=LABEL_SIZE)
+        ax.set_ylabel(r"$x$", size=LABEL_SIZE)
+        return im
+
+
+class HeatNet(PDE_Net):
+    def __init__(self, units: List[int], activation: Callable):
+        super().__init__(units, activation)
+
+    def set_problem(self, c: float, initial_condition: Callable, space: Tuple[float, float], T: float) -> HeatNet:
         self.c = c
         self.u0 = initial_condition
         self.space = space
         self.T = T
         return self
 
-    def forward(self, x, t):
-        input = torch.hstack((x.reshape(-1, 1), t.reshape(-1, 1)))
-        return self.layers(input)
-
-    def sample_data(self):
-        N = 1000
-        N_TEST = int(0.1 * N)
-
-        xl, xr = self.space
-        x_train = (xl + (xr - xl) * torch.rand(N, 1)).requires_grad_(True)
-        t_train = (self.T * torch.rand(N, 1)).requires_grad_(True)
-        x_test = (xl + (xr - xl) * torch.rand(N_TEST, 1)).requires_grad_(True)
-        t_test = (self.T * torch.rand(N_TEST, 1)).requires_grad_(True)
-
-        return x_train, t_train, x_test, t_test
-
-    def loss(self, x, t):
+    def loss(self, x: Tensor, t: Tensor) -> Tensor:
         output = self(x, t)
 
         # Physics.
@@ -64,47 +129,71 @@ class PDENet(nn.Module):
 
         return physics_loss + init_loss + bound_loss
 
-    def train(self, n_epochs, batch_size, lr):
-        x_train, t_train, x_test, t_test = self.sample_data()
 
-        n_batches = x_train.size(dim=0) // batch_size
-        idx = np.arange(x_train.size(dim=0))
+class AdvectionNet(PDE_Net):
+    def __init__(self, units: List[int], activation: Callable):
+        super().__init__(units, activation)
 
-        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+    def set_problem(self, c: float, initial_condition: Callable, space: Tuple[float, float], T: float) -> HeatNet:
+        self.c = c
+        self.u0 = initial_condition
+        self.space = space
+        self.T = T
+        return self
 
-        self.hist = {"Train": [], "Test": []}
-        epoch_range = trange(n_epochs, desc="Learning PDE")
-        for _ in epoch_range:
-            np.random.shuffle(idx)
-            for b in range(n_batches):
-                batch = idx[b * batch_size: (b + 1) * batch_size]
+    def loss(self, x: Tensor, t: Tensor) -> Tensor:
+        output = self(x, t)
 
-                loss = self.loss(x_train[batch], t_train[batch])
-                loss.backward()
+        # Physics.
+        dx = grad(output, x, torch.ones_like(output), create_graph=True)[0]
+        dt = grad(output, t, torch.ones_like(output), create_graph=True)[0]
+        physics_loss = torch.mean((dt + self.c * dx) ** 2)
 
-                optimizer.step()
-                optimizer.zero_grad()
+        # Initial condition.
+        t_init = torch.zeros_like(x)
+        init_loss = torch.mean((self(x, t_init) - self.u0(x)) ** 2)
 
-            train_loss, test_loss = self.eval(x_train, t_train, x_test, t_test)
-            epoch_range.set_postfix(train=train_loss, test=test_loss)
+        # Boundary condition.
+        xl, xr = self.space
+        xl_ = xl * torch.ones_like(t)
+        xr_ = xr * torch.ones_like(t)
+        bound_loss = torch.mean((self(xl_, t) - self(xr_, t)) ** 2)
 
-    def eval(self, x_train, t_train, x_test, t_test):
-        train_loss = self.loss(x_train, t_train).item()
-        test_loss = self.loss(x_test, t_test).item()
-        self.hist["Train"].append(train_loss)
-        self.hist["Test"].append(test_loss)
-        return train_loss, test_loss
+        return physics_loss + init_loss + bound_loss
 
-    def plot_solution(self, ax):
-        n = 100
-        space = torch.linspace(*self.space, n)
-        time = torch.linspace(0, self.T, n)
 
-        space_, time_ = torch.meshgrid(space, time, indexing="ij")
-        ext = [time.min(), time.max(), space.min(), space.max()]
+class EigenNet(ANN):
+    def __init__(self, units: List[int], activation: Callable):
+        super().__init__(units, activation)
 
-        pred = self(space_.reshape(-1, 1), time_.reshape(-1, 1)).detach()
-        im = ax.imshow(pred.reshape(n, n), extent=ext, aspect="auto", origin="lower", cmap="coolwarm")
-        ax.set_xlabel(r"$t$", size=LABEL_SIZE)
-        ax.set_ylabel(r"$x$", size=LABEL_SIZE)
-        return im
+    def set_problem(self, A: np.ndarray) -> EigenNet:
+        self.A = torch.tensor(A).float()
+        self.n = A.shape[0]
+        return self
+
+    def forward(self, t: Tensor) -> Tensor:
+        return self.layers(t)
+
+    def sample_data(self) -> Tuple[Tuple[Tensor], Tuple[Tensor]]:
+        T = 5
+        t_train = (T * torch.rand(N, 1)).requires_grad_(True)
+        t_test = (T * torch.rand(N_TEST, 1)).requires_grad_(True)
+        return (t_train,), (t_test,)
+
+    def loss(self, t: Tensor) -> Tensor:
+        x = self(t)
+
+        xx = torch.sum(x ** 2, axis=1)
+        xAx = torch.sum(x.T * torch.matmul(self.A, x.T), axis=0)
+        xxA = torch.einsum('i,jk->ijk', xx, self.A)
+        xAxI = torch.einsum('i,jk->ijk', xAx, torch.eye(self.n))
+        f = torch.einsum('ijk,ik->ij', xxA - xAxI, x)
+
+        # Physics.
+        dt = grad(x, t, torch.ones_like(x), create_graph=True)[0]
+        physics_loss = torch.mean((dt - f) ** 2)
+
+        # Initial condition.
+        init_loss = torch.mean((self(torch.zeros(1).reshape(-1, 1)) - torch.ones(self.n)) ** 2)
+
+        return physics_loss + init_loss
