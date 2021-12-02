5d351a7a40e61d17efc8b768310dabb4a89a8d6d
Stjepan Salatovic <s.salatovic@yahoo.de>
2021-12-02 17:55:48 +0100

    Plot exact heat and avection solution for different 'c > 0', and 1D plot for different time levels.

--- project3/aux_scripts/adv_exact.py
+++ project3/aux_scripts/adv_exact.py
@@ -5,14 +5,20 @@ import numpy as np
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from src.finite_differences import ThreePoint
-from src.utils import LABEL_SIZE
+from src.utils import LABEL_SIZE, LEGEND_SIZE
 
-SHOW_PLOTS = True
+
+def plot_3d():
+    ax = plt.axes(projection='3d')
+    ax.set_xlabel(r"$x$", size=LABEL_SIZE)
+    ax.set_ylabel(r"$t$", size=LABEL_SIZE)
+    ax.set_zlabel(r"$u(x,t)$", size=LABEL_SIZE, rotation=90)
+    ax.view_init(elev=20., azim=25)
+    ax.zaxis.set_rotate_label(False)
+    return ax
 
 
 # DATA. Speed coefficient. Initial datum.
-c = 2.
-
 J, N = 500, 500
 space = np.linspace(0, 1, J + 2)
 time = np.linspace(0, .5, N + 1)
@@ -22,18 +28,48 @@ u0 = lambda x: np.where((0.3 < x) & (x < 0.6), 1, 0)
 
 # FDM. Implicit Euler + 2. order central
 fdm = ThreePoint((0, [-1, 1, 0]))
-fdm.solve(c, u0(space), space, time)
+exact_sol_ = lambda c: fdm.solve(c, u0(space), space, time)
 
 
 # EXACT SOLUTION.
 X, T = np.meshgrid(space, time)
 
-ax = plt.axes(projection='3d')
+
+# PLOT. 3D.
+c = 0.5
+fdm.solve(c, u0(space), space, time)
+ax = plot_3d()
 ax.plot_surface(X.T, T.T, fdm.sol, alpha=.9, cmap="jet")
-ax.set_xlabel(r"$x$", size=LABEL_SIZE)
-ax.set_ylabel(r"$t$", size=LABEL_SIZE)
-ax.set_zlabel(r"$u(x,t)$", size=LABEL_SIZE, rotation=90)
-ax.view_init(elev=20., azim=25)
-ax.zaxis.set_rotate_label(False)
 plt.tight_layout()
+if not os.path.exists("project3/figs/"):
+    os.makedirs("project3/figs/")
+plt.savefig("project3/figs/adv_exact_1.pdf", bbox_inches='tight', format="pdf")
+plt.show()
+
+c = 2.0
+fdm.solve(c, u0(space), space, time)
+ax = plot_3d()
+ax.plot_surface(X.T, T.T, fdm.sol, alpha=.9, cmap="jet")
+plt.tight_layout()
+if not os.path.exists("project3/figs/"):
+    os.makedirs("project3/figs/")
+plt.savefig("project3/figs/adv_exact_2.pdf", bbox_inches='tight', format="pdf")
+plt.show()
+
+
+# PLOT. 1D.
+time_points = [.0, .1, .2, .3, .4, .5]
+idx = [np.argmin(np.abs(time - t)) for t in time_points]
+
+color=plt.cm.jet(np.linspace(0, 1, len(time_points)))[::-1]
+for i, (ix, t) in enumerate(zip(idx, time_points)):
+    plt.plot(space, fdm.sol[:, ix], c=color[i], label=rf"$t={t:.1f}$")
+
+plt.xlabel(r"$x$", size=LABEL_SIZE)
+plt.ylabel(r"$u(x, t)$", size=LABEL_SIZE)
+plt.legend(fontsize=LEGEND_SIZE)
+plt.tight_layout()
+if not os.path.exists("project3/figs/"):
+    os.makedirs("project3/figs/")
+plt.savefig("project3/figs/adv_diff_times.pdf", bbox_inches='tight', format="pdf")
 plt.show()
--- project3/aux_scripts/heat_exact.py
+++ project3/aux_scripts/heat_exact.py
@@ -4,31 +4,65 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-from src.utils import LABEL_SIZE
-
-SHOW_PLOTS = True
+from src.utils import LABEL_SIZE, LEGEND_SIZE
 
 
-# DATA. Diffusion coefficient. Initial datum.
-c = 1.
+def plot_3d():
+    ax = plt.axes(projection='3d')
+    ax.set_xlabel(r"$x$", size=LABEL_SIZE)
+    ax.set_ylabel(r"$t$", size=LABEL_SIZE)
+    ax.set_zlabel(r"$u(x,t)$", size=LABEL_SIZE, rotation=90)
+    ax.view_init(elev=20., azim=40)
+    ax.zaxis.set_rotate_label(False)
+    return ax
 
+
+# DATA. Initial datum.
 J, N = 100, 100
 space = np.linspace(0, 1, J + 2)
 time = np.linspace(0, .5, N + 1)
 
 u0 = lambda x: np.sin(np.pi * x)
 
+
 # EXACT SOLUTION.
 X, T = np.meshgrid(space, time)
+exact_sol_ = lambda c: np.exp(-c * np.pi ** 2 * T.T) * np.sin(np.pi * X.T)
 
-exact_sol = np.exp(-np.pi ** 2 * T.T) * np.sin(np.pi * X.T)
 
-ax = plt.axes(projection='3d')
+# PLOT. 3D.
+exact_sol = exact_sol_(c=0.2)
+ax = plot_3d()
 ax.plot_surface(X.T, T.T, exact_sol, alpha=.9, cmap="coolwarm")
-ax.set_xlabel(r"$x$", size=LABEL_SIZE)
-ax.set_ylabel(r"$t$", size=LABEL_SIZE)
-ax.set_zlabel(r"$u(x,t)$", size=LABEL_SIZE, rotation=90)
-ax.view_init(elev=20., azim=40)
-ax.zaxis.set_rotate_label(False)
 plt.tight_layout()
+if not os.path.exists("project3/figs/"):
+    os.makedirs("project3/figs/")
+plt.savefig("project3/figs/heat_exact_1.pdf", bbox_inches='tight', format="pdf")
+plt.show()
+
+exact_sol = exact_sol_(c=1.0)
+ax = plot_3d()
+ax.plot_surface(X.T, T.T, exact_sol, alpha=.9, cmap="coolwarm")
+plt.tight_layout()
+if not os.path.exists("project3/figs/"):
+    os.makedirs("project3/figs/")
+plt.savefig("project3/figs/heat_exact_2.pdf", bbox_inches='tight', format="pdf")
+plt.show()
+
+
+# PLOT. 1D.
+time_points = [.0, .1, .2, .3]
+idx = [np.argmin(np.abs(time - t)) for t in time_points]
+
+color=plt.cm.coolwarm(np.linspace(0, 1, len(time_points)))[::-1]
+for i, (ix, t) in enumerate(zip(idx, time_points)):
+    plt.plot(space, exact_sol[:, ix], c=color[i], label=rf"$t={t:.1f}$")
+
+plt.xlabel(r"$x$", size=LABEL_SIZE)
+plt.ylabel(r"$u(x, t)$", size=LABEL_SIZE)
+plt.legend(fontsize=LEGEND_SIZE)
+plt.tight_layout()
+if not os.path.exists("project3/figs/"):
+    os.makedirs("project3/figs/")
+plt.savefig("project3/figs/heat_diff_times.pdf", bbox_inches='tight', format="pdf")
 plt.show()
