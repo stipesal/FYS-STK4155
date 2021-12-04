ff6e5a83d1412d2e7e132fa601dd874d549541af
Stjepan Salatovic <s.salatovic@yahoo.de>
2021-12-04 09:16:54 +0100

    Use FTBS instead of BTBS for calculating 'exact' advection solution.

--- project3/aux_scripts/adv_exact.py
+++ project3/aux_scripts/adv_exact.py
@@ -18,28 +18,31 @@ def plot_3d():
     return ax
 
 
-# DATA. Speed coefficient. Initial datum.
-J, N = 500, 500
+# DATA.
+T = .5
+J = 500
+N = lambda c, J: 2 * np.ceil(c * T * (J + 1)).astype(int)
 space = np.linspace(0, 1, J + 2)
-time = np.linspace(0, .5, N + 1)
+time = lambda c: np.linspace(0, T, N(c, J) + 1)
 
 u0 = lambda x: np.where((0.3 < x) & (x < 0.6), 1, 0)
 
 
-# FDM. Implicit Euler + 1. order upwind.
-fdm = ThreePoint((0, [-1, 1, 0]))
-exact_sol_ = lambda c: fdm.solve(c, u0(space), space, time)
+# FDM. Explicit Euler + 1. order upwind (FTBS).
+fdm = ThreePoint((1, [-1, 1, 0]))
+exact_sol_ = lambda c: fdm.solve(c, u0(space), space, time(c))
 
 
 # EXACT SOLUTION.
-X, T = np.meshgrid(space, time)
+X, T_ = np.meshgrid(space, time)
 
 
 # PLOT. 3D.
 c = 0.5
-fdm.solve(c, u0(space), space, time)
+fdm.solve(c, u0(space), space, time(c))
+X, T_ = np.meshgrid(space, time(c))
 ax = plot_3d()
-ax.plot_surface(X.T, T.T, fdm.sol, alpha=.8, cmap="jet")
+ax.plot_surface(X.T, T_.T, fdm.sol, alpha=.8, cmap="jet")
 ax.plot_wireframe(
     space.reshape(-1, 1), np.zeros(space.size).reshape(-1, 1), u0(space).reshape(-1, 1), color="k", lw=2.5,
 )
@@ -50,9 +53,10 @@ plt.savefig("project3/figs/adv_exact_1.pdf", bbox_inches='tight', format="pdf")
 plt.show()
 
 c = 2.0
-fdm.solve(c, u0(space), space, time)
+fdm.solve(c, u0(space), space, time(c))
+X, T_ = np.meshgrid(space, time(c))
 ax = plot_3d()
-ax.plot_surface(X.T, T.T, fdm.sol, alpha=.8, cmap="jet")
+ax.plot_surface(X.T, T_.T, fdm.sol, alpha=.8, cmap="jet")
 ax.plot_wireframe(
     space.reshape(-1, 1), np.zeros(space.size).reshape(-1, 1), u0(space).reshape(-1, 1), color="k", lw=2.5,
 )
@@ -64,12 +68,12 @@ plt.show()
 
 
 # PLOT. 1D.
-time_points = [.0, .1, .2, .3, .4, .5]
-idx = [np.argmin(np.abs(time - t)) for t in time_points]
+time_points = [.0, .05, .25, 0.5]
+idx = [np.argmin(np.abs(time(c) - t)) for t in time_points]
 
 color=plt.cm.jet(np.linspace(0, 1, len(time_points)))[::-1]
 for i, (ix, t) in enumerate(zip(idx, time_points)):
-    plt.plot(space, fdm.sol[:, ix], c=color[i], label=rf"$t={t:.1f}$")
+    plt.plot(space, fdm.sol[:, ix], c=color[i], label=rf"$t={t:.2f}$")
 
 plt.xlabel(r"$x$", size=LABEL_SIZE)
 plt.ylabel(r"$u(x, t)$", size=LABEL_SIZE)
